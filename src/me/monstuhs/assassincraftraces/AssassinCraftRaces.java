6ed54a19383199d7ec0ee5143ca14b489a14536c
James <jamestwilson@live.com>
2012-06-05 22:05:33 -0700

    initial creation

---
+++ src/me/monstuhs/assassincraftraces/AssassinCraftRaces.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.monstuhs.assassincraftraces;
+
+/**
+ *
+ * @author James
+ */
+public class AssassinCraftRaces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
